2e0700391503004d20106515f0a8fb438685b2e4
Ignacio Cimadevila Tornos <nacho@estudio143.com>
2021-02-02 18:06:46 +0100

    more stuff

--- notification-script.js
+++ notification-script.js
@@ -9,34 +9,32 @@ let alarmsActives = [];
 let alarmsOld = [];
 let id = 0;
 
+
 function getListeners() {
   btnCreateAlarm.addEventListener('click', createAlarm);
 }
 
-function askNotification() {
-  if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    const notification = new Notification("Hi there!");
-  }
-
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification("Hi there!");
-      }
-    });
-  }
+function getInitialData() {
+  alarmsActives = JSON.parse(localStorage.getItem('actives')) || [];
+  alarmsOld = JSON.parse(localStorage.getItem('olds')) || [];
+  
+  renderLists();
 }
 
-function getInitialStatus() {
-  alarmsActives = JSON.parse(localStorage.getItem('actives')) || [];
-  
+function renderLists() {
+  elAlarmsActive.innerHTML = '';
+  elAlarmsOld.innerHTML = '';
   if (alarmsActives.length === 0) {
     elAlarmsActive.insertAdjacentHTML('beforeend', '<p>No alarms created yet</p>');
   } else {
-    setAlarms();
+    id = alarmsActives.length;
+    setAlarmsDOM(alarmsActives, elAlarmsActive);
+  }
+
+  if (alarmsOld.length === 0) {
+    elAlarmsOld.insertAdjacentHTML('beforeend', '<p>No old alarms</p>');
+  } else {
+    setAlarmsDOM(alarmsOld, elAlarmsOld);
   }
 }
 
@@ -57,10 +55,39 @@ function createAlarm(e) {
     }
 
     storeAlarm(objAlarm);
-    renderTemplate(objAlarm);
+    renderTemplate(objAlarm, elAlarmsActive);
     cleanForm();
-    new Notification('Alarm created');
     id ++;
+  }
+}
+
+function storeAlarm(objAlarm) {
+  alarmsActives.push(objAlarm);
+  const alarms = JSON.stringify(alarmsActives);
+  localStorage.setItem('actives', alarms);
+}
+
+function renderTemplate(obj, container) {
+  let editBtn;
+  let deleteBtn;
+  const templateNewAlarm = `<div class="alarm">
+    <span class="date">${obj.date} ${obj.hour}:${obj.minute}</span>
+    <span class="text">${obj.desc}</span>
+    <div id="buttons${obj.id}"></div>
+  </div>`;
+  
+  container.insertAdjacentHTML('beforeend', templateNewAlarm);
+
+  if (container.classList.contains('alarms__active')) {
+    editBtn = document.createElement('button');
+    deleteBtn = document.createElement('button');
+    editBtn.id = 'editAlarm' + obj.id;
+    editBtn.innerHTML = 'Edit';
+    deleteBtn.id = 'deleteAlarm' + obj.id;
+    deleteBtn.innerHTML = 'Delete';
+  
+    document.getElementById('buttons' + obj.id).appendChild(editBtn);
+    document.getElementById('buttons' + obj.id).appendChild(deleteBtn);
   }
 }
 
@@ -75,42 +102,50 @@ function cleanForm() {
   elMinute.value = '';
 }
 
-function renderTemplate(obj) {
-  let editBtn;
-  let deleteBtn;
-  const templateNewAlarm = `<div class="alarm">
-    <span class="date">${obj.date} ${obj.hour}:${obj.minute}</span>
-    <span class="text">${obj.desc}</span>
-    <div id="buttons${obj.id}"></div>
-  </div>`;
+function setAlarmsDOM(alarms, container) {
   
-  elAlarmsActive.insertAdjacentHTML('beforeend', templateNewAlarm);
-
-  editBtn = document.createElement('button');
-  deleteBtn = document.createElement('button');
-  editBtn.id = 'editAlarm' + id;
-  editBtn.innerHTML = 'Edit';
-  deleteBtn.id = 'deleteAlarm' + id;
-  deleteBtn.innerHTML = 'Delete';
-
-  document.getElementById('buttons' + id).appendChild(editBtn);
-  document.getElementById('buttons' + id).appendChild(deleteBtn);
-}
-
-function setAlarms() {
-  for(alarm of alarmsActives) {
-    renderTemplate(alarm);
-    id ++;
+  for(alarm of alarms) {
+    renderTemplate(alarm, container);
+    if (container.classList.contains('alarms__active')) {
+      id ++;
+    }
   }
 }
 
-function storeAlarm(objAlarm) {
-  alarmsActives.push(objAlarm);
-  const alarms = JSON.stringify(alarmsActives);
-  localStorage.setItem('actives', alarms);
+function checkAlarms() {
+  const now = new Date();
+  const minuteCheck = now.getMinutes() > 9 ? now.getMinutes() : '0' + now.getMinutes()
+  const hourCheck = now.getHours() > 9 ? now.getHours() : '0' + now.getHours();
+  const dayCheck = now.getDate() > 9 ? now.getDate() : '0' + now.getDate();
+  const monthCheck = now.getMonth() > 9 ? now.getMonth() + 1 : '0' + (now.getMonth() + 1);
+  const yearCheck = now.getFullYear();
+
+  for(al of alarmsActives) {
+    if (al.date === `${yearCheck}-${monthCheck}-${dayCheck}` && al.hour === hourCheck.toString() && al.minute === minuteCheck.toString()) {
+      sendNotification(al.desc);
+      removeNotification(al);
+    }
+  }
 }
 
-askNotification();
+function sendNotification(text) {
+  if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
+       new Notification(text);
+      }
+    });
+  }
+}
+
+function removeNotification(alarm) {
+  const index = alarmsActives.findIndex((al) => al.id === alarm.id);
+  alarmsActives.splice(index, 1);
+  alarmsOld.push(alarm);
+  renderLists();
+}
+
 getListeners();
-getInitialStatus();
+getInitialData();
+setInterval(checkAlarms, 1000);
 
